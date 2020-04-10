0eccde77704f2d91e900121b470075536025ae42
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-10 11:26:32 -0400

    Players imported in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
+import { Players } from "./components/Players";
 
 function App() {
   return (
