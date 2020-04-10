a4753ee46e0217136feee2b649d3e295a9581cd6
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-10 12:21:48 -0400

    data showing

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from "axios"
+import axios from "axios";
 import { Players } from "./components/Players";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log(res);
+        this.setState({players: res.data});
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="App">
+        <h1>Players</h1>
+        {this.state.players.map(player => <li key={player.id}>
+         
+        <h2>Name: {player.name}</h2>
+        <h3>Country: {player.country}</h3>
+        <h4>Searchs: {player.searches}</h4>
+        <h5>Id: {player.id}</h5>
+        </li>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+
--- client/src/components/Players.js
+++ client/src/components/Players.js
@@ -1,10 +1,28 @@
-import React from "react";
+// import React from "react";
 
-export function Players(props) {
-  console.log(props);
-  return (
-    <div>
+// export function Players(props) {
+//   console.log(props);
+//   return (
+//     <div>
+//          <p>
+//         {this.props.player.name}
+//       </p>
     
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
+
+// class Players extends React.Component {
+//     render() {
+//       return(
+//         <div>
+//             <p>{props.players.name}</p>
+          
+    
+//         </div>
+//       );
+//     }
+//   }
+  
+  
+//   export default Players; 
