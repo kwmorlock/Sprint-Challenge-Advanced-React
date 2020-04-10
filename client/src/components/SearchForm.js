f58b6390977d5be73a3ab341d711ff08daf1d9c4
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-04-10 13:17:37 -0400

    dark mode working

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
-
+import { useForm } from "../Hooks/UseForm";
+import { useDarkMode } from "../Hooks/useDarkMode";
 export function SearchForm(props) {
-  const [form, setForm] = useState("");
-  // console.log(form);
-  const handleChange = e => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+
+
+  const [darkMode, setDarkMode] = useDarkMode("isDark");
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
   };
+  const [form, handleChange] = useForm();
   const handleSubmit = e => {
     e.preventDefault();
     props.handleSearch(form.search);
   };
   return (
     <form onSubmit={handleSubmit}>
+      <div onClick={toggleMode}>DarkMode</div>
       <input
         name="search"
         placeholder="Search"
         onChange={handleChange}
         value={form.search}
       />
+   
       <button type="submit">Submit</button>
     </form>
   );
